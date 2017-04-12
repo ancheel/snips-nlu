ba67bd65cd7e5660cc0b856d4a1a3a21f0f47d9b
Alice <alice.coucke@gmail.com>
2017-04-12 11:48:22 +0200

    finish rasa nlu engine

--- snips_nlu/rasa_nlu_engine.py
+++ snips_nlu/rasa_nlu_engine.py
@@ -1,68 +1,49 @@
-from abc import ABCMeta, abstractmethod
+import io
+import json
+import os
+import shutil
 
-from dataset import validate_and_format_dataset, filter_dataset
-from snips_nlu.built_in_entities import BuiltInEntity
-from snips_nlu.constants import (
-    USE_SYNONYMS, SYNONYMS, DATA, INTENTS, ENTITIES, UTTERANCES,
-    LANGUAGE, VALUE, AUTOMATICALLY_EXTENSIBLE, ENTITY, BUILTIN_PARSER,
-    CUSTOM_PARSERS, CUSTOM_ENGINE)
-from snips_nlu.intent_classifier.snips_intent_classifier import \
-    SnipsIntentClassifier
-from snips_nlu.intent_parser.builtin_intent_parser import BuiltinIntentParser
-from snips_nlu.intent_parser.crf_intent_parser import CRFIntentParser
-from snips_nlu.intent_parser.regex_intent_parser import RegexIntentParser
-from snips_nlu.languages import Language
+from rasa_nlu.config import RasaNLUConfig
+from rasa_nlu.model import Metadata, Interpreter
+from rasa_nlu.model import Trainer
+
+from dataset import validate_and_format_dataset
+from snips_nlu.nlu_engine import NLUEngine
+from snips_nlu.result import IntentClassificationResult
 from snips_nlu.result import ParsedSlot
 from snips_nlu.result import Result
-from snips_nlu.slot_filler.crf_tagger import CRFTagger, default_crf_model
-from snips_nlu.slot_filler.crf_utils import TaggingScheme
-from snips_nlu.slot_filler.feature_functions import crf_features
-from snips_nlu.utils import instance_from_dict
+from snips_nlu.utils import transform_to_rasa_format
 
 
 class RasaNLUEngine(NLUEngine):
-    def __init__(self, backend="spacy_sklearn", language, builtin_parser=None, custom_parsers=None,
-                 entities=None):
-        super(RasaNLUEngine, self).__init__(language)
-
-        from rasa_nlu.converters import load_data
-        from rasa_nlu.config import RasaNLUConfig
-        from rasa_nlu.model import Trainer
-
-        self._builtin_parser = None
-        self.builtin_parser = builtin_parser
-        self.custom_parsers = custom_parsers
-        self.entities = entities
-
-    @property
-    def builtin_parser(self):
-        return self._builtin_parser
-
-    @builtin_parser.setter
-    def builtin_parser(self, value):
-        if value is not None \
-                and value.parser.language != self.language.iso_code:
-            raise ValueError(
-                "Built in parser language code ('%s') is different from "
-                "provided language code ('%s')"
-                % (value.parser.language, self.language.iso_code))
-        self._builtin_parser = value
+    def __init__(self, language, backend):
+        super(RasaSpacyNLUEngine, self).__init__(language)
+        self.backend = backend
+        self.config_file_name = None
+        self.interpreter = None
+        self.language = language
 
     def parse(self, text):
         """
         Parse the input text and returns a dictionary containing the most
         likely intent and slots.
         """
-        if self.builtin_parser is None and self.custom_parsers is None:
-            raise ValueError("NLUEngine as no built-in parser nor "
-                             "custom parsers")
-        parsers = []
-        if self.custom_parsers is not None:
-            parsers += self.custom_parsers
-        if self.builtin_parser is not None:
-            parsers.append(self.builtin_parser)
+        res = self.interpreter.parse(unicode(text))
+        intent_res = IntentClassificationResult(res['intent']['name'],
+                                                res['intent']['confidence'])
+        slots = res['entities']
+        valid_slot = []
+        for slot in slots:
+            slot_value = slot['value']
+            slot_name = slot['name']
+            match_range = [slot['start'], slot['end']]
 
-        return _parse(text, parsers, self.entities).as_dict()
+            s = ParsedSlot(match_range, slot_value, 'whathever', slot_name)
+
+            valid_slot.append(s)
+
+        return Result(text, parsed_intent=intent_res,
+                      parsed_slots=valid_slot).as_dict()
 
     def fit(self, dataset):
         """
@@ -71,75 +52,46 @@ class RasaNLUEngine(NLUEngine):
         intents.
         See https://github.com/snipsco/snips-nlu/blob/develop/README.md for
         details about the format.
-        :return: A fitted SnipsNLUEngine
+        :return: A fitted RasaNLUEngine
         """
         dataset = validate_and_format_dataset(dataset)
-        custom_dataset = filter_dataset(dataset, CUSTOM_ENGINE)
-        custom_parser = RegexIntentParser().fit(dataset)
-        self.entities = snips_nlu_entities(dataset)
-        taggers = dict()
-        for intent in custom_dataset[INTENTS].keys():
-            intent_custom_entities = get_intent_custom_entities(custom_dataset,
-                                                                intent)
-            features = crf_features(intent_custom_entities,
-                                    language=self.language)
-            taggers[intent] = CRFTagger(default_crf_model(), features,
-                                        TaggingScheme.BIO, self.language)
-        intent_classifier = SnipsIntentClassifier(self.language)
-        crf_parser = CRFIntentParser(intent_classifier, taggers).fit(dataset)
-        self.custom_parsers = [custom_parser, crf_parser]
-        return self
 
-    def to_dict(self):
-        """
-        Serialize the SnipsNLUEngine to a json dict, after having reset the
-        builtin intent parser. Thus this serialization, contains only the
-        custom intent parsers.
-        """
-        language_code = None
-        if self.language is not None:
-            language_code = self.language.iso_code
+        # data
+        if len(dataset['intents']) < 2:
+            raise ValueError(
+                "Rasa backend can only be used on a dataset containing multiple"
+                " intents")
 
-        return {
-            LANGUAGE: language_code,
-            CUSTOM_PARSERS: [p.to_dict() for p in self.custom_parsers],
-            BUILTIN_PARSER: None,
-            ENTITIES: self.entities
+        dir_name = '__rasa_tmp'
+        if not os.path.exists(dir_name):
+            os.makedirs(dir_name)
+
+        training_data = transform_to_rasa_format(dataset)
+        training_file_name = os.path.join(dir_name, "training_data.json")
+        with io.open(training_file_name, 'w', encoding='utf8') as f:
+            json.dumps(training_data)
+
+        config_dict = {
+            "pipeline": self.backend,
+            "mitie_file": "./data/total_word_feature_extractor.dat",
+            "path": dir_name,
+            "data": training_file_name
         }
 
-    @classmethod
-    def load_from(cls, language, customs=None, builtin_path=None,
-                  builtin_binary=None):
-        """
-        Create a `SnipsNLUEngine` from the following attributes
+        config_file_name = os.path.join(dir_name, "config.json")
+        with io.open(config_file_name, 'w', encoding='utf8') as f:
+            json.dumps(config_dict)
 
-        :param language: ISO 639-1 language code or Language instance
-        :param customs: A `dict` containing custom intents data
-        :param builtin_path: A directory path containing builtin intents data
-        :param builtin_binary: A `bytearray` containing builtin intents data
-        """
+        trainer = Trainer(RasaNLUConfig(config_file_name))
+        trainer.train(training_data)
+        trainer.persist(dir_name, persistor=None,
+                        create_unique_subfolder=False)
 
-        if isinstance(language, (str, unicode)):
-            language = Language.from_iso_code(language)
+        metadata = Metadata.load(dir_name)
 
-        custom_parsers = None
-        entities = None
-        if customs is not None:
-            custom_parsers = [instance_from_dict(d) for d in
-                              customs[CUSTOM_PARSERS]]
-            entities = customs[ENTITIES]
-        builtin_parser = None
-        if builtin_path is not None or builtin_binary is not None:
-            builtin_parser = BuiltinIntentParser(language=language,
-                                                 data_path=builtin_path,
-                                                 data_binary=builtin_binary)
+        self.interpreter = Interpreter.load(metadata,
+                                            RasaNLUConfig(config_file_name))
 
-        return cls(language, builtin_parser=builtin_parser,
-                   custom_parsers=custom_parsers, entities=entities)
+        shutil.rmtree(dir_name)
 
-    def __eq__(self, other):
-        return isinstance(other, self.__class__) and \
-               self.to_dict() == other.to_dict()
-
-    def __ne__(self, other):
-        return not self.__eq__(other)
+        return self
