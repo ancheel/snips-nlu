8fc290ca1e337ac7eecc19ef07f7717777bf1343
Sylvain Raybaud <sylvain.raybaud@snips.ai>
2017-04-19 18:21:53 +0200

    encoding + cosmetic changes

--- snips_nlu/translator/apis/gcloud_translator.py
+++ snips_nlu/translator/apis/gcloud_translator.py
@@ -1,13 +1,16 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+
 from google.cloud import translate
 
 from .abstract_translator import Translator
 
 
 class GcloudTranslator(Translator):
-
-	def __init__(self, model="nmt"):
-		self.client = translate.Client()
-		self.model = model
-
-	def translate(self, text, source_language , target_language):
-		return self.client.translate(text, target_language=target_language, model=self.model)['translatedText']
+    def __init__(self, model="nmt"):
+        self.client = translate.Client()
+        self.model = model
+    
+    def translate(self, text, source_language, target_language):
+        return self.client.translate(text, target_language=target_language, model=self.model)['translatedText']
