2bb0769d623392598d83ec98037b3cbc1859b782
Sylvain Raybaud <sylvain.raybaud@snips.ai>
2017-04-19 14:55:35 +0200

    add query parameter for choosing model (neural or phrase based)

--- snips_nlu/translator/apis/gcloud_translator.py
+++ snips_nlu/translator/apis/gcloud_translator.py
@@ -5,9 +5,9 @@ from .abstract_translator import Translator
 
 class GcloudTranslator(Translator):
 
-	def __init__(self):
+	def __init__(self, model="nmt"):
 		self.client = translate.Client()
-
+		self.model = model
 
 	def translate(self, text, source_language , target_language):
-		return self.client.translate(text, target_language=target_language)['translatedText']
+		return self.client.translate(text, target_language=target_language, model=self.model)['translatedText']
