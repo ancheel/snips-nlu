332ccc5da4e968cf88726539c580f042a11b28cf
Sylvain Raybaud <sylvain.raybaud@snips.ai>
2017-04-19 18:22:13 +0200

    encoding + cosmetic changes

--- snips_nlu/translator/apis/abstract_translator.py
+++ snips_nlu/translator/apis/abstract_translator.py
@@ -1,4 +1,9 @@
-class Translator():
+# -*- coding: utf-8 -*-
 
-	def translate(self, source_language, target_language):
-		NotImplementedError('Abstract method `translate` is not defined.')
+from __future__ import unicode_literals
+
+
+class Translator():
+	
+    def translate(self, source_language, target_language):
+        NotImplementedError('Abstract method `translate` is not defined.')
