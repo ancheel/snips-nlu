97080e5a70d0f1691e76d823adb45eb87a1da24a
Sylvain Raybaud <sylvain.raybaud@snips.ai>
2017-04-25 17:34:46 +0200

    first embedding routine

---
+++ snips_nlu/embedding.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+import urllib
+from snips_nlu.tokenization import tokenize
+
+class Embedding():
+    
+    def __init__(self, endpoint):
+        self.endpoint = endpoint
+    
+    def similarity(self, seq1, seq2):
+        
+        arg1 = tokenize(seq1) if isinstance(seq1, str) else seq1
+        arg2 = tokenize(seq2) if isinstance(seq2, str) else seq2
+        
+        query_part_1 = "&".join( [ urllib.urlencode( { "ws1": token } ) for token in arg1 ] )
+        query_part_2 = "&".join( [ urllib.urlencode( { "ws2": token } ) for token in arg2 ] )
+        
+        url = self.endpoint + "?" + query_part_1 + "&" + query_part_2
+        
+        result = float(urllib.urlopen(url).read())
+        
+        return result
