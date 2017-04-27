c1eff678a4d2276733c14459f09452aa34fc8dca
Sylvain Raybaud <sylvain.raybaud@snips.ai>
2017-04-27 18:15:39 +0200

    make embedding class more robust to encoding problems and error messages from the server

--- snips_nlu/embedding.py
+++ snips_nlu/embedding.py
@@ -1,7 +1,12 @@
 # -*- coding: utf-8 -*-
 
+from __future__ import unicode_literals
+
 import urllib
 from snips_nlu.tokenization import tokenize
+import json
+import re
+float_re = re.compile(r"-?\d*\.\d*|NaN")
 
 class Embedding():
     
@@ -13,8 +18,18 @@ class Embedding():
         arg1 = tokenize(seq1) if isinstance(seq1, str) else seq1
         arg2 = tokenize(seq2) if isinstance(seq2, str) else seq2
         
-        query_part_1 = "&".join( [ urllib.urlencode( { "ws1": token } ) for token in arg1 ] )
-        query_part_2 = "&".join( [ urllib.urlencode( { "ws2": token } ) for token in arg2 ] )
+        query_part_1 = "&".join(
+            [
+                urllib.urlencode( { "ws1": token.encode("iso-8859-15", errors="replace") } )
+                for token in arg1
+            ]
+        )
+        query_part_2 = "&".join(
+            [
+                urllib.urlencode( { "ws2": token.encode("iso-8859-15", errors="replace") } )
+                for token in arg2
+            ]
+        )
         
         url = self.endpoint\
               + "/n_similarity"\
@@ -23,6 +38,20 @@ class Embedding():
               + "&"\
               + query_part_2
         
-        result = float(urllib.urlopen(url.lower()).read())
+        url = url.lower()
+        
+        result = urllib.urlopen(url).read()
+        
+        if float_re.match(result):
+            return float(result)
+        
+        try:
+            status = json.loads(result)["status"]
+            if status == 500:
+                return 0.
+            else:
+                raise Exception("Embedding server replied with status {}: {}".format(status, result["message"]))
+        except:
+            raise Exception("Could not get similarity: "+str(result))
         
         return result
